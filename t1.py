06da71434931f30ca0b21a532341b7b3301df26b
Björn Stenberg <bjorn@haxx.se>
2018-12-20 16:00:40 +0100

    Add button trigger

--- t1.py
+++ t1.py
@@ -7,18 +7,22 @@ import os
 broker="digi"
 pump_ip=""
 mqtt = paho.Client("client-001")
+pump_running = False
 
 def on_message(client, userdata, message):
     global pump_ip
     topic = message.topic.decode("utf-8")
     payload = message.payload.decode("utf-8")
 
-    print(topic + ": " + payload)
+    print("mqtt in : " + topic + ": " + payload)
 
     if topic == "node0/info":
         data = json.loads(payload)
         pump_ip = data["ip_addr"]
-        print "node0 address is " + pump_ip
+    elif topic == "node0/pump/activated":
+        global pump_running
+        pump_running = (payload == "1")
+
 
 def mqtt_init():
     mqtt.on_message = on_message
@@ -34,7 +38,7 @@ def pump_start(pump_on = True):
         print "No pump in system"
         return
     url = "http://[" + pump_ip + "]/v1/pumps/0"
-    print("Calling " + url)
+    print("http out: " + url)
     try:
         response = requests.put(url,
                                 json = { "activated": pump_on,
@@ -62,20 +66,36 @@ def led(on=True):
     with open('/sys/class/gpio/gpio75/value', 'w') as file:
         file.write("%d" % on*1)
 
+def button_init():
+    if not os.access('/sys/class/gpio/', os.W_OK):
+        return
+    if not os.path.isdir("/sys/class/gpio/gpio67"):
+        with open('/sys/class/gpio/export', 'w') as file:
+            file.write("67")
+    with open('/sys/class/gpio/gpio67/direction', 'w') as file:
+            file.write("in")
+
+def button_read():
+    with open('/sys/class/gpio/gpio67/value', 'r') as file:
+        val = file.read(1)
+    return val == "0"
 
 #### main ###
 
 led_init()
+button_init()
 mqtt_init()
 
 try:
     while True:
-        time.sleep(2)
-        led(True)
-        pump_start(True)
-        time.sleep(2)
-        pump_start(False)
-        led(False)
+        if button_read():
+            if not pump_running:
+                led(True)
+                pump_start(True)
+        elif pump_running:
+            led(False)
+            pump_start(False)
+        time.sleep(0.5)
 
 except:
     print("exiting... ")
